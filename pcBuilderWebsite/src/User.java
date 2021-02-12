996e17894646cc3272ab658cdb29ef6e8e03ee19
Daniel Trksak <60165871+dTrksak@users.noreply.github.com>
2021-02-11 18:53:14 -0600

    add setUserInfo()

--- pcBuilderWebsite/src/User.java
+++ pcBuilderWebsite/src/User.java
@@ -5,7 +5,7 @@ public class User {
 	String last_name = null;
 	String email = null;
 	
-	User(int user_id, String first_name, String last_name, String email) {
+	public void setUserInfo(int user_id, String first_name, String last_name, String email) {
 		this.user_id = user_id;
 		this.first_name = first_name;
 		this.last_name = last_name;
