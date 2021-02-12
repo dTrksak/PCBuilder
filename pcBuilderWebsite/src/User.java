fc5a31e396930cca85fd1a6c0b61dd77b5a90ca3
WilliamKXiong <mana_grey@hotmail.com>
2021-02-11 19:30:22 -0600

    getters and setters

--- pcBuilderWebsite/src/User.java
+++ pcBuilderWebsite/src/User.java
@@ -11,5 +11,36 @@ public class User {
 		this.last_name = last_name;
 		this.email = email;
 	}
-	
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
