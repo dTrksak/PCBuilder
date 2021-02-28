b2b238789cebcb936f59ad8fa44f89b5f0e7e3d4
WilliamKXiong <mana_grey@hotmail.com>
2021-02-27 18:09:00 -0600

    index/homepage comments

--- PCBuilder/src/main/java/com/pcbuilder/presentation/HomePage.java
+++ PCBuilder/src/main/java/com/pcbuilder/presentation/HomePage.java
@@ -8,9 +8,12 @@ public class HomePage {
 
 	@RequestMapping("/")
 	public String index() {
+		//replace "blank.html" with homepage html file name
+		//"blank.html" located in src/main/resources/templates
 		return "blank.html";
 	}
 	
+	//this can be removed once a homepage has been created
 	@RequestMapping("/index")
 	public String index2() {
 		return "blank.html";
