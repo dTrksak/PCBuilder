bba272a2ce548ffc6ff2a56da2b081a600af857d
Daniel Trksak <60165871+dTrksak@users.noreply.github.com>
2021-03-25 18:16:42 -0500

    another update to signin controller
    edited the signin html so password is not null in the database
    added comments

--- PCBuilder/src/main/java/com/pcbuilder/controllers/SignupController.java
+++ PCBuilder/src/main/java/com/pcbuilder/controllers/SignupController.java
@@ -34,19 +34,25 @@ public class SignupController {
 		//if has user with same email
 		if(user != null) {
 			//update user to mysql
+			System.out.println("fail"); //Here because the html page doesn't show up
 		  	return "signup_fail"; //=signup_fail.html
 		}
-	
+		
+		//get a connection for interacting with the database
 		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/pc_build", "root", "admin");
 		Statement stmt = conn.createStatement();
+		
+		//find the next user id
 		String nextIDSQL = "select max(user_id) from user;"; //find the next user id to use
 		ResultSet rs = stmt.executeQuery(nextIDSQL);
 		rs.next();
-		int nextID = rs.getInt(1);
+		int nextID = rs.getInt(1)+1;
 		
-		String sql = "insert into user (user_id, first_name, last_name, email, password) values ("+nextID+1+", '"+input.getFirstName()+"', '"+input.getLastName()+"', '"+input.getEmail()+"', '"+input.getPassword()+"');";
+		//add the new user
+		String sql = "insert into user (user_id, first_name, last_name, email, password) values ("+nextID+", '"+input.getFirstName()+"', '"+input.getLastName()+"', '"+input.getEmail()+"', '"+input.getPassword()+"');";
 		stmt.executeUpdate(sql);
 		
+		System.out.println("success"); //Here because the html page doesn't show up
 		return "signup_success"; //=signup_success.html
   	}
 }
