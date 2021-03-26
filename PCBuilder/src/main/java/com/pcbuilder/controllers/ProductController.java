ae3dd2173abbd817ec406761160397e0d9dc2247
WilliamKXiong <mana_grey@hotmail.com>
2021-03-25 20:42:05 -0500

    removed unused code

--- PCBuilder/src/main/java/com/pcbuilder/controllers/ProductController.java
+++ PCBuilder/src/main/java/com/pcbuilder/controllers/ProductController.java
@@ -23,22 +23,6 @@ public class ProductController {
 	@Autowired
 	private CategoryRepository categoryRepo;
 	
-	@GetMapping("/all")
-	@ResponseBody
-	public List<Product> list(){
-		return productRepo.findAll();
-	}
-	
-	@GetMapping("/cpu")
-	@ResponseBody
-	public List<Product> cpu(){
-		List<Product> product = productRepo.findByCategory(categoryRepo.findByCategoryName("Cpu"));
-		//System.out.println(product.getCpuCoolers().get(0).getCpuCoolerId());
-		//List<CpuCooler> cpuList = product.getCpuCoolers();
-		//System.out.println(cpuList.get(0));
-		return product;
-	}
-	
 	@GetMapping("")
 	@ResponseBody
 	public List<Product> returnByCategory(@RequestParam(value="category", required=true) String categoryName) {
