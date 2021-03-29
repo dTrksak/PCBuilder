b2d6d934593b3b4ec76370cfcabf7c0c417c8bfa
WilliamKXiong <mana_grey@hotmail.com>
2021-03-28 19:07:09 -0500

    ability to preview html pages

--- PCBuilder/src/main/java/com/pcbuilder/presentation/HomePage.java
+++ PCBuilder/src/main/java/com/pcbuilder/presentation/HomePage.java
@@ -1,6 +1,7 @@
 package com.pcbuilder.presentation;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,10 +20,8 @@ public class HomePage {
 		return "index.html";
 	}
 	
-
-	//this can be removed once a homepage has been created
-	@RequestMapping("/index1")
-	public String index1() {
-		return "index1.html";
+	@RequestMapping("/{page}")
+	public String getPage(@PathVariable String page) {
+		return page + ".html";
 	}
 }
